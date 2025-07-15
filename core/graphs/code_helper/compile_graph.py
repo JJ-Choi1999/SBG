21ea233bae9dc09f9e1873cf924a7bdd86fa8de8
JJ-Choi1999 <2486595624@qq.com>
2025-07-15 10:26:38 +0800

    实现 code_helper graph enable_mutual = False 时知识库的写入和更新

--- core/graphs/code_helper/compile_graph.py
+++ core/graphs/code_helper/compile_graph.py
@@ -1,9 +1,15 @@
+import os.path
 import sys
+import time
+from itertools import chain
 from pathlib import Path
 import uuid
 import warnings
 
 from weaviate.config import AdditionalConfig, Timeout
+
+from common.error.load import UnLoadableError
+from common.file.file import iter_file_infos
 
 sys.path.append(str(Path(__file__).parent.parent.parent.parent))
 
@@ -125,6 +131,14 @@ class CompileGraph:
                 chunk_overlap=self.__chunk_overlap
             )
 
+
+            workspace = init_result.data_source.workspace
+            file_paths = init_result.data_source.file_paths
+            enable_knowledge = init_result.global_setting.enable_knowledge
+
+            if self.__enable_mutual and enable_knowledge and workspace and file_paths:
+                self.update_vector_data(index_name=workspace, file_paths=file_paths)
+
             # Step 2: ExecGraph
             self.__max_retry = init_result.global_setting.max_retry
             exec_result = self.compile_and_run(
@@ -157,6 +171,37 @@ class CompileGraph:
     def __close_vector(self):
         if not self.__vector_store:
             self.__vector_store.close()
+
+    def update_vector_data(self, index_name: str, file_paths: list[str]):
+        """
+        更新向量数据库指定索引数据
+        :param index_name: 索引名
+        :param file_paths: 作为更新源数据的文件列表
+        :return:
+        """
+        print(f'=' * 100)
+        split_docs_map = {}
+
+        for file_index, file_path in enumerate(file_paths):
+            try:
+                split_docs_map[file_path] = self.__vector_store.load_file(
+                    file_path=file_path,
+                    file_type=os.path.splitext(file_path)[-1][1:],
+                    chunk_size=self.__chunk_size,
+                    chunk_overlap=self.__chunk_overlap
+                )
+                print(f'【{file_index + 1}/{len(file_paths)}】已添加文件: {file_path}')
+
+            except UnLoadableError as e:
+                print(f'【{file_index + 1}/{len(file_paths)}】文件: {file_path} 出现异常: {str(e)}')
+
+        print(f'* 文件正在写入知识库...')
+        s_time = time.time()
+        split_docs = list(chain.from_iterable(list(split_docs_map.values())))
+        if index_name in self.__vector_store.all_collections():
+            self.__vector_store.delete_collection(collection_name=index_name)
+        self.__vector_store.init_vector(split_docs=split_docs, index_name=index_name)
+        print(f'* 文件写入知识库完成, 耗时: 【{time.time() - s_time}(s)】')
 
 if __name__ == '__main__':
     __enable_mutual = YAML_CONFIGS_INFO['code_helper']['mutual_config']['enable_mutual']
