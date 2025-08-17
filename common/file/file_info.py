e2ea266792a22f405b633ff1ba5de830686fe86b
JJ-Choi1999 <2486595624@qq.com>
2025-08-17 16:09:14 +0800

    调整代码

--- common/file/file_info.py
+++ common/file/file_info.py
@@ -14,7 +14,7 @@ def extract_file_type(file_path: str) -> dict:
         'mime': kind.mime
     }
 
-
+# [todo] 需要补全其它的文件信息提取
 def extract_file_info(file_path: str) -> dict:
     """
     提取文件信息, 输入文件地址, 返回文件信息
